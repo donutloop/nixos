71faf5bfaab812113bde28894fce49f585ff473f
donutloop <marcel.edmund.franke@gmail.com>
2020-09-16 18:44:32 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,70 +1,19 @@
 
-{ config, pkgs, ... }:
-
 {
-  imports = [ <nixpkgs/nixos/modules/installer/virtualbox-demo.nix> ];
-
-  environment.systemPackages = with pkgs; [
-  vim
-  git
-  wget]; 
-
-    #...
-  # Can be consistently pinned at a certain revision with `rev = ""`
   inputs.lightmeter = { type = "github"; owner = "ngi-nix"; repo = "lightmeter"; };
-  #...
-  # Or just `{ self, nixpkgs, lightmeter }` but that doesn't cover all cases
   outputs = { self, nixpkgs, ... }@inputs:
 
 
   nixosConfigurations.<name> = nixpkgs.lib.nixosSystem {
     system = "<system>";
     modules = [
-      #...
       ({ ... }: {
         imports = builtins.attrValues inputs.lightmeter.nixosModules;
         nixpkgs.overlays = [ inputs.lightmeter.overlay ];
         services.lightmeter.enable = true;
         services.lightmeter.flags.watch_dir = "/etc/postfix";
       })
-      #...
     ];
   };
-  
-
-
-  # Let demo build as a trusted user.
-# nix.trustedUsers = [ "demo" ];
-
-# Mount a VirtualBox shared folder.
-# This is configurable in the VirtualBox menu at
-# Machine / Settings / Shared Folders.
-# fileSystems."/mnt" = {
-#   fsType = "vboxsf";
-#   device = "nameofdevicetomount";
-#   options = [ "rw" ];
-# };
-
-# By default, the NixOS VirtualBox demo image includes SDDM and Plasma.
-# If you prefer another desktop manager or display manager, you may want
-# to disable the default.
-# services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
-# services.xserver.displayManager.sddm.enable = lib.mkForce false;
-
-# Enable GDM/GNOME by uncommenting above two lines and two lines below.
-# services.xserver.displayManager.gdm.enable = true;
-# services.xserver.desktopManager.gnome3.enable = true;
-
-# Set your time zone.
-# time.timeZone = "Europe/Amsterdam";
-
-# List packages installed in system profile. To search, run:
-# \$ nix search wget
-# environment.systemPackages = with pkgs; [
-#   wget vim
-# ];
-
-# Enable the OpenSSH daemon.
-# services.openssh.enable = true;
-
+ 
 }
